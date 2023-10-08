86cd005958a2a89ca6e313c341d77e58dc23c185
rickyurvinaUCN <ricardo.urvina@alumnos.ucn.cl>
2023-10-08 19:26:06 -0300

    Function Sum

--- app/src/main/java/com/example/calculator/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/calculator/ui/home/HomeFragment.java
@@ -4,32 +4,56 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
 import com.example.calculator.databinding.FragmentHomeBinding;
+
+import org.w3c.dom.Text;
 
 public class HomeFragment extends Fragment {
 
-private FragmentHomeBinding binding;
+    private FragmentHomeBinding binding;
+
+    private EditText numberOne;
+    private EditText numberTwo;
+    private TextView txtResult;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-    binding = FragmentHomeBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        numberOne = binding.numberOne;
+        numberTwo = binding.numberTwo;
+        txtResult = binding.txtResult;
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
-@Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnSum.setOnClickListener(v -> {
+            sum();
+        });
+
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void sum() {
+        int num1 = Integer.parseInt(numberOne.getText().toString());
+        int num2 = Integer.parseInt(numberTwo.getText().toString());
+        int result = num1 + num2;
+        txtResult.setText(String.valueOf(result));
     }
 }
