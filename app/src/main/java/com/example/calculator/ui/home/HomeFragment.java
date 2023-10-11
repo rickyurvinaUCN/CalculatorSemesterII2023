2742cf302217d99954a3829fdc7380ea9c1f584d
rickyurvinaUCN <ricardo.urvina@alumnos.ucn.cl>
2023-10-10 22:23:17 -0300

    Calculator with spinner

--- app/src/main/java/com/example/calculator/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/calculator/ui/home/HomeFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -19,18 +20,44 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-    private EditText numberOne;
-    private EditText numberTwo;
-    private TextView txtResult;
+    private EditText txtNumber1, txtNumber2;
+    private TextView txtResult;;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        numberOne = binding.numberOne;
-        numberTwo = binding.numberTwo;
+        txtNumber1 = binding.txtNum1;
+        txtNumber2 = binding.txtNum2;
         txtResult = binding.txtResult;
+        binding.btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sum();
+            }
+        });
+
+        binding.btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sub();
+            }
+        });
+
+        binding.btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mul();
+            }
+        });
+
+        binding.btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                div();
+            }
+        });
 
         return root;
     }
@@ -50,10 +77,37 @@ public class HomeFragment extends Fragment {
         binding = null;
     }
 
-    public void sum() {
-        int num1 = Integer.parseInt(numberOne.getText().toString());
-        int num2 = Integer.parseInt(numberTwo.getText().toString());
+    private void sum() {
+        int num1 = Integer.parseInt(txtNumber1.getText().toString());
+        int num2 = Integer.parseInt(txtNumber2.getText().toString());
         int result = num1 + num2;
         txtResult.setText(String.valueOf(result));
+    }
+
+    public void sub() {
+        int num1 = Integer.parseInt(txtNumber1.getText().toString());
+        int num2 = Integer.parseInt(txtNumber2.getText().toString());
+        int result = num1 - num2;
+        txtResult.setText(String.valueOf(result));
+    }
+
+    public void mul() {
+        int num1 = Integer.parseInt(txtNumber1.getText().toString());
+        int num2 = Integer.parseInt(txtNumber2.getText().toString());
+        int result = num1 * num2;
+        txtResult.setText(String.valueOf(result));
+    }
+
+    public void div() {
+        int num1 = Integer.parseInt(txtNumber1.getText().toString());
+        int num2 = Integer.parseInt(txtNumber2.getText().toString());
+        if (num2 != 0) {
+            double sum = num1 / num2;
+            String res = String.valueOf(sum);
+            txtResult.setText(String.valueOf(res));
+        } else {
+            String message = "No es posible dividir para 0";
+            Toast.makeText(this.getContext(), message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
